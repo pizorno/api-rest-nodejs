9868f883e6d064d04bf769b6d4f87ddc0db30f43
Eduardo Pizorno <pizorno@gmail.com>
2024-03-26 09:45:19 -0300

    add POST route transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,24 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoute(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionSchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
