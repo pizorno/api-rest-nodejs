042f411d5b89c65e08d4b63f5366f77460516318
Eduardo Pizorno <pizorno@gmail.com>
2024-03-25 23:06:19 -0300

    change error on validate env variables with zod

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,4 +11,7 @@ export const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('Invalid environment variables!', _env.error.format())
+  throw new Error('Invalid environment variables!')
 }
+
+export const env = _env.data
